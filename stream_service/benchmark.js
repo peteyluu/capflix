ba0b3de33d5d08c98130f1d5cf3966df4c80bd74
Felipe Catania <felicatania@gmail.com>
2018-02-08 15:03:24 -0800

    Modify Siege stress test

--- stream_service/benchmark.js
+++ stream_service/benchmark.js
@@ -2,6 +2,21 @@ const siege = require('siege');
 
 siege()
   .on(3000)
+  .concurrent(20)
   .for(120).seconds
-  .get('/chunk/somecontent?res=720&chunkNumber=1')
+  .get('/chunk/somecontent?res=240&chunkNumber=23')
   .attack();
+
+// siege()
+//   .on(3000)
+//   .concurrent(20)
+//   .for(120).seconds
+//   .get('/manifest/somecontent?res=720')
+//   .attack();
+
+// siege()
+//   .on(3000)
+//   .concurrent(100)
+//   .for(30).seconds
+//   .post('/startsession')
+//   .attack();
