9385652a4066a8ae389d07b62a006079f0848407
Felipe Catania <felicatania@gmail.com>
2018-02-14 10:55:07 -0800

    Add final modifications and docker-compose

--- stream_service/benchmark.js
+++ stream_service/benchmark.js
@@ -1,22 +1,22 @@
 const siege = require('siege');
 
-siege()
-  .on(3000)
-  .concurrent(20)
-  .for(120).seconds
-  .get('/chunk/somecontent?res=240&chunkNumber=23')
-  .attack();
-
 // siege()
-//   .on(3000)
+//   .on(1337)
 //   .concurrent(20)
 //   .for(120).seconds
-//   .get('/manifest/somecontent?res=720')
+//   .get('http://ec2-13-57-179-177.us-west-1.compute.amazonaws.com:1337/chunk/somecontent?res=240&chunkNumber=23')
 //   .attack();
 
 // siege()
-//   .on(3000)
-//   .concurrent(100)
-//   .for(30).seconds
-//   .post('/startsession')
+//   .on(1337)
+//   .concurrent(20)
+//   .for(120).seconds
+//   .get('http://ec2-13-57-179-177.us-west-1.compute.amazonaws.com:1337/manifest/somecontent?res=720')
 //   .attack();
+
+siege()
+  .on(3000)
+  .concurrent(100)
+  .for(30).seconds
+  .post('http://ec2-13-57-179-177.us-west-1.compute.amazonaws.com:1337/startsession', {content_id: 123, user_id: 456})
+  .attack();
