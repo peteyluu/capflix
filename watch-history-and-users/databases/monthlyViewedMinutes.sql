52b95d1894ba30a4f86ed6babbd1e856a869177d
Jimmy Tran <jimmyctran2013@gmail.com>
2018-02-14 19:09:54 -0800

    Altered user sessions database model to optimize reads and aggregating data

--- watch-history-and-users/databases/monthlyViewedMinutes.sql
+++ watch-history-and-users/databases/monthlyViewedMinutes.sql
@@ -1,6 +1,12 @@
 CREATE TABLE monthly_viewed_minutes (
-	date varchar(10) PRIMARY KEY,
+	date varchar(30),
 	category varchar(250) NOT NULL,
 	original_content boolean NOT NULL,
-	viewed_minutes numeric(300, 2) not null
+	viewed_minutes numeric(300, 2) NOT NULL,
+	PRIMARY KEY (date, category, original_content)	
 );
+
+Create Table categories (
+	categoryId serial PRIMARY KEY,
+	category varchar(250) NOT NULL UNIQUE
+)
