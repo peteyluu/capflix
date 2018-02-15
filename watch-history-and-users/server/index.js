68c2e18f6b5425f849495ca051a86cd36e1f9bdb
Jimmy Tran <jimmyctran2013@gmail.com>
2018-02-14 19:04:49 -0800

    Added code that checks SQS for new messages to write into user sessions database

--- watch-history-and-users/server/index.js
+++ watch-history-and-users/server/index.js
@@ -3,7 +3,11 @@ const express = require('express');
 const postgres = require('../databases/postgres');
 const bodyParser  = require('body-parser');
 const cassandra = require('../databases/cassandra');
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').load();
+}
 const AWS = require('aws-sdk');
+AWS.config.update({ accessKeyId: process.env.aws_access_key_id, secretAccessKey: process.env.aws_secret_access_key, region: 'us-west-1'});
 const sqs = new AWS.SQS({ apiVersion: '2012-11-05' });
 
 
@@ -36,31 +40,60 @@ app.post('/signup', (req, res) => {
 	})
 })
 
+var recieveMessageParams = {
+	QueueUrl: 'https://sqs.us-west-1.amazonaws.com/567607828756/test_user_sessions_standard_queue',
+	MaxNumberOfMessages: 1,
+	MessageAttributeNames: [
+		'All'
+	]
+}
+
+setInterval(() => {
+	sqs.receiveMessage(recieveMessageParams, (err, data) => {
+		if (err) console.log(err, err.stack);
+		else {
+			if (data.Messages) {
+				for (var i = 0; i < data.Messages.length; i++) {
+					var receiptHandle = data.Messages[i].ReceiptHandle;
+					var messageAttribute = data.Messages[i].MessageAttributes;
+					var sessionInfo = {session_id: messageAttribute.session_id.StringValue,
+														 user_id: JSON.parse(messageAttribute.user_id.StringValue),
+														 content_id: JSON.parse(messageAttribute.content_id.StringValue),
+														 category: messageAttribute.category.StringValue,
+														 original_content: JSON.parse(messageAttribute.original_content.StringValue),
+														 viewed_minutes: JSON.parse(messageAttribute.viewed_minutes.StringValue),
+														 geolocation: JSON.parse(messageAttribute.geolocation.StringValue), 
+														 date: messageAttribute.date.StringValue
+														};
+					cassandra.addUserSession(sessionInfo, (err, data) => {
+						if (err) {
+							console.error('ERROR', err);
+						} else {
+							console.log('Data successfully added to database');
+							var deleteParams = {
+								QueueUrl: 'https://sqs.us-west-1.amazonaws.com/567607828756/test_user_sessions_standard_queue',
+								ReceiptHandle: receiptHandle
+							}
+							sqs.deleteMessage(deleteParams, (err, deleteData) => {
+								if (err) console.log(err, err.stack);
+								else {
+									console.log('Data removed from queue', deleteData);
+								}
+							})
+						}
+					})
+				}
+			}
+		}
+	});
+}, 2000);
+
+
 app.get('/', (req, res) => {
 	res.send(201);
 })
 
-// app.get('/login/:email/:password', (req, res) => {
-// 	postgres.selectUserByEmail(req.params.email, (err, data) => {
-// 		if (err) {
-// 			console.error('ERROR:', err);
-// 		} else {
-// 			console.log(data);
-// 			res.send();
-// 		}
-// 	})
-// });
 
-// app.get('/test', (req, res) => {
-// 	cassandra.selectUserSession((err, data) => {
-// 		if (err) {
-// 			console.error('ERROR:', err);
-// 		} else {
-// 			console.log(data.rows[0])
-// 			res.send(data.rows[0]);
-// 		}
-// 	})
-// });
 if (!module.parent) {
 	app.listen(app.get('port'), function() {
 	  console.log('server listen on port ' + app.get('port'));
